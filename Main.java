8fbb2af355495188b66bebeff0b0bd2f80a94821
kunalghatode16 <158257966+kunalghatode16@users.noreply.github.com>
2024-04-30 13:20:33 +0530

    11

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World 10, Tried scm polling #10 ");
+		System.out.println("Hello World 10, Tried scm polling #10");
 	}
 }
 
